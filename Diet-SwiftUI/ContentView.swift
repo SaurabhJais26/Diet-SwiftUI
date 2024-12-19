c143a3dc80f8a34cb2a39671426aa0e58b2b32ca
Saurabh Jaiswal <sjsaurabhjais@gmail.com>
2024-12-19 16:24:43 +0530

    Initial Commit

---
+++ Diet-SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Diet-SwiftUI
+//
+//  Created by Saurabh Jaiswal on 19/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Diet-SwiftUI/Diet_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Diet_SwiftUIApp.swift
+//  Diet-SwiftUI
+//
+//  Created by Saurabh Jaiswal on 19/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Diet_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
